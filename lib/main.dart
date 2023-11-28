7c96ce58912d1f18227ba1865fd68a09008af83f
Do Phu Quy <quydp@hybrid-technologies.co.jp>
2023-11-28 09:08:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'screens/welcome/welcome_screen.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This is the free verstion of Flutter Chat App UI Kit
+  // Full code: https://www.patreon.com/posts/chat-app-ui-for-93094811
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Flutter Way',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      themeMode: ThemeMode.light,
+      home: const WelcomeScreen(),
+    );
+  }
+}
